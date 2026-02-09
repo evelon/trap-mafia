a35026c567db600d217bf91166e0ffd786ee6cf7
Jonghyun Lim <ezemango@gmail.com>
2026-02-10 05:40:31 +0900

    test: pre-commit test. merge ㄴㄴ

--- apps/backend/alembic/env.py
+++ apps/backend/alembic/env.py
@@ -1,12 +1,11 @@
 import os
 from logging.config import fileConfig
 
-from sqlalchemy import engine_from_config
-from sqlalchemy import pool
+from sqlalchemy import engine_from_config, pool
 
+import app.models  # noqa: F401
 from alembic import context
 from app.db.base import Base
-import app.models  # noqa: F401
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
