4e6e1485f3ce7dc42bfe689bec2de2ffe98ab7ce
Jonghyun Lim <ezemango@gmail.com>
2026-03-17 18:52:09 +0900

    test(be): CasePlayerRepo.create_many / .list_by_case_id

---
+++ apps/backend/app/repositories/case_player.py
@@ -0,0 +1,12 @@
+from typing import Any
+
+
+class CasePlayerRepo:
+    def __init__(self, *args, **kwargs):
+        pass
+
+    async def create_many(self, *args, **kwargs) -> Any:
+        pass
+
+    async def list_by_case_id(self, *args, **kwargs) -> Any:
+        pass
